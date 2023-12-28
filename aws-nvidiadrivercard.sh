7988cef3d3e97bf71147151dfa5550de061d235f
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-12-28 03:27:37 +0300

    Just aws driver

--- aws-nvidiadrivercard.sh
+++ aws-nvidiadrivercard.sh
@@ -1,5 +1,7 @@
-apt update -y
-export DEBIAN_FRONTEND=noninteractive
-apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" install nvidia-driver-535 -y
-
-
+#apt update -y
+#export DEBIAN_FRONTEND=noninteractive
+#apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" install nvidia-driver-535 -y
+BASE_URL=https://us.download.nvidia.com/tesla
+DRIVER_VERSION=450.80.02
+curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
+sh NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
