219857f29e3468d26e7959eb7737d4275335ab1d
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-12-28 03:28:54 +0300

    Just aws driver

--- aws-nvidiadrivercard.sh
+++ aws-nvidiadrivercard.sh
@@ -5,3 +5,4 @@ BASE_URL=https://us.download.nvidia.com/tesla
 DRIVER_VERSION=450.80.02
 curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
 sh NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
+reboot
